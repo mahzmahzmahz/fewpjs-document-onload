bcc99b74ce3eb246f2a6ea04ec01af40ece45dd7
Alex Marz <alexhmarz@gmail.com>
2021-02-03 18:11:12 -0500

    Things are starting to click again

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+        const text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+    console.log("The DOM has loaded");
+  });
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  
